e63e05e11e160d9863a8a4246b87878207b57696
wushuc <wushuc@student.ethz.ch>
2019-06-03 16:48:22 +0200

    solved indentation issue

--- code/CIFAR_baseline_training.py
+++ code/CIFAR_baseline_training.py
@@ -23,8 +23,8 @@ import torchvision
 import model as models
 
 model_names = sorted(name for name in models.__dict__
-                     if name.islower() and not name.startswith("__")
-                     and callable(models.__dict__[name]))
+    if name.islower() and not name.startswith("__")
+    and callable(models.__dict__[name]))
 
 parser = argparse.ArgumentParser(description='PyTorch ImageNet Training')
 parser.add_argument('data', metavar='DIR',
@@ -32,8 +32,8 @@ parser.add_argument('data', metavar='DIR',
 parser.add_argument('-a', '--arch', metavar='ARCH', default='resnet18',
                     choices=model_names,
                     help='model architecture: ' +
-                    ' | '.join(model_names) +
-                    ' (default: resnet18)')
+                        ' | '.join(model_names) +
+                        ' (default: resnet18)')
 parser.add_argument('-j', '--workers', default=4, type=int, metavar='N',
                     help='number of data loading workers (default: 4)')
 parser.add_argument('--epochs', default=80, type=int, metavar='N',
@@ -43,8 +43,8 @@ parser.add_argument('--start-epoch', default=0, type=int, metavar='N',
 parser.add_argument('-b', '--batch-size', default=4, type=int,
                     metavar='N',
                     help='mini-batch size (default: 256), this is the total '
-                    'batch size of all GPUs on the current node when '
-                    'using Data Parallel or Distributed Data Parallel')
+                         'batch size of all GPUs on the current node when '
+                         'using Data Parallel or Distributed Data Parallel')
 parser.add_argument('--lr', '--learning-rate', default=0.001, type=float,
                     metavar='LR', help='initial learning rate', dest='lr')
 parser.add_argument('--momentum', default=0.9, type=float, metavar='M',
@@ -72,7 +72,7 @@ best_acc1 = 0
 
 def main():
     args = parser.parse_args()
-    
+
     if args.seed is not None:
         random.seed(args.seed)
         torch.manual_seed(args.seed)
@@ -82,9 +82,9 @@ def main():
                       'which can slow down your training considerably! '
                       'You may see unexpected behavior when restarting '
                       'from checkpoints.')
-    
-    
-    args.world_size = 1
+
+
+    args.world_size = 1  
 
 
     ngpus_per_node = torch.cuda.device_count()
@@ -95,15 +95,15 @@ def main():
 
 def main_worker(args):
     global best_acc1
-    
+
     # create model
     if args.pretrained:
         print("=> using pre-trained model '{}'".format(args.arch))
         model = models.__dict__[args.arch](pretrained=True)
     else:
         print("=> creating model '{}'".format(args.arch))
-        model = models.v1resnet18(n_img_per_batch = args.batch_size, n_freq  = 12, n_orient = 8, n_phase = 2, imsize = 32,num_classes=10)
-
+        print('batch size is ',args.batch_size)
+        model = models.resnet18(num_classes=10)
 
 
     device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
@@ -112,10 +112,10 @@ def main_worker(args):
     # define loss function (criterion) and optimizer
     criterion = nn.CrossEntropyLoss().to(device)
 
-optimizer = torch.optim.SGD(model.parameters(), args.lr,
-                            momentum=args.momentum,
-                            weight_decay=args.weight_decay)
-    
+    optimizer = torch.optim.SGD(model.parameters(), args.lr,
+                                momentum=args.momentum,
+                                weight_decay=args.weight_decay)
+
     # optionally resume from a checkpoint
     if args.resume:
         if os.path.isfile(args.resume):
@@ -132,58 +132,52 @@ optimizer = torch.optim.SGD(model.parameters(), args.lr,
         else:
             print("=> no checkpoint found at '{}'".format(args.resume))
 
-cudnn.benchmark = True
-    
-    # Data loading code
-    # """TODO: add gray values"""
-    
-    # normalize = transforms.Normalize(mean=[0.485, 0.456, 0.406],
-    #                                  std=[0.229, 0.224, 0.225])
-    
-    # """TODO: add gray values"""
-    
-    
-    transform = transforms.Compose(
-                                   [transforms.Grayscale(),transforms.ToTensor(),
-                                    transforms.Normalize((0.5,), (0.5,))]) # transforms sequentially, mean and variance prespecified
-                                    
-                                    trainset = torchvision.datasets.CIFAR10(root='./data', train=True,
-                                                                            download=False, transform=transform)
-                                    
-                                    train_loader = torch.utils.data.DataLoader(trainset, batch_size=4,
-                                                                               shuffle=False, num_workers=2)
-                                    
-                                    val_set = torchvision.datasets.CIFAR10(root='./data', train=False,
-                                                                           download=False, transform=transform)
-                                    val_loader = torch.utils.data.DataLoader(val_set, batch_size=4,
-                                                                             shuffle=False, num_workers=2)
-                                    
-                                    classes = ('plane', 'car', 'bird', 'cat',
-                                               'deer', 'dog', 'frog', 'horse', 'ship', 'truck')
-                                    
-                                    if args.evaluate:
-                                        validate(val_loader, model, criterion, args)
-                                            return
+    cudnn.benchmark = True
 
-for epoch in range(args.start_epoch, args.epochs):
-    adjust_learning_rate(optimizer, epoch, args)
-        
+    # """TODO: add gray values"""
+
+
+    transform = transforms.Compose(
+        [transforms.ToTensor(),
+         transforms.Normalize((0.5,0.5,0.5), (0.5,0.5,0.5))]) # transforms sequentially, mean and variance prespecified 
+
+    trainset = torchvision.datasets.CIFAR10(root='./data', train=True,
+                                            download=False, transform=transform)
+
+    train_loader = torch.utils.data.DataLoader(trainset, batch_size=4,
+                                              shuffle=False, num_workers=2)
+
+    val_set = torchvision.datasets.CIFAR10(root='./data', train=False,
+                                           download=False, transform=transform)
+    val_loader = torch.utils.data.DataLoader(val_set, batch_size=4,
+                                             shuffle=False, num_workers=2)
+
+    classes = ('plane', 'car', 'bird', 'cat',
+               'deer', 'dog', 'frog', 'horse', 'ship', 'truck')
+
+    if args.evaluate:
+        validate(val_loader, model, criterion, args)
+        return
+
+    for epoch in range(args.start_epoch, args.epochs):
+        adjust_learning_rate(optimizer, epoch, args)
+
         # train for one epoch
         train(train_loader, model, criterion, optimizer, epoch, args)
-        
+
         # evaluate on validation set
         acc1 = validate(val_loader, model, criterion, args)
-        
+
         # remember best acc1 and save checkpoint
         is_best = acc1 > best_acc1
         best_acc1 = max(acc1, best_acc1)
-        
+
         save_checkpoint({
-                        'epoch': epoch + 1,
-                        'state_dict': model.state_dict(),
-                        'accuracy': best_acc1,
-                        'optimizer' : optimizer.state_dict(),
-                        }, is_best)
+            'epoch': epoch + 1,
+            'state_dict': model.state_dict(),
+            'accuracy': best_acc1,
+            'optimizer' : optimizer.state_dict(),
+        }, is_best)
 
 
 def train(train_loader, model, criterion, optimizer, epoch, args):
@@ -194,42 +188,42 @@ def train(train_loader, model, criterion, optimizer, epoch, args):
     top5 = AverageMeter('Acc@5', ':6.2f')
     progress = ProgressMeter(len(train_loader), batch_time, data_time, losses, top1,
                              top5, prefix="Epoch: [{}]".format(epoch))
-        
-                             # switch to train mode
-                             model.train()
-                             
-                             end = time.time()
-                             for i, (input, target) in enumerate(train_loader):
-                                 # measure data loading time
-                                 data_time.update(time.time() - end)
-                                     # TODO:
-                                     device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
-                                         # input = input.cuda(args.gpu, non_blocking=True)
-                                         # target = target.cuda(args.gpu, non_blocking=True)
-                                         input = input.to(device).contiguous()
-                                             target = target.to(device)
-                                                 # compute output
-                                                 #print(input.dtype)
-                                                 output = model(input)
-                                                     loss = criterion(output, target)
-                                                         
-                                                         # measure accuracy and record loss
-                                                         acc1, acc5 = accuracy(output, target, topk=(1, 5))
-                                                             losses.update(loss.item(), input.size(0))
-                                                                 top1.update(acc1[0], input.size(0))
-                                                                     top5.update(acc5[0], input.size(0))
-                                                                         
-                                                                         # compute gradient and do SGD step
-                                                                         optimizer.zero_grad()
-                                                                             loss.backward()
-                                                                                 optimizer.step()
-                                                                                     
-                                                                                     # measure elapsed time
-                                                                                     batch_time.update(time.time() - end)
-                                                                                         end = time.time()
-                                                                                             
-                                                                                             if i % args.print_freq == 0:
-                                                                                                 progress.print(i)
+
+    # switch to train mode
+    model.train()
+
+    end = time.time()
+    for i, (input, target) in enumerate(train_loader):
+        # measure data loading time
+        data_time.update(time.time() - end)
+        # TODO:
+        device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+        # input = input.cuda(args.gpu, non_blocking=True)
+        # target = target.cuda(args.gpu, non_blocking=True)
+        input = input.to(device).contiguous()
+        target = target.to(device)
+        # compute output
+        #print(input.dtype)
+        output = model(input)
+        loss = criterion(output, target)
+
+        # measure accuracy and record loss
+        acc1, acc5 = accuracy(output, target, topk=(1, 5))
+        losses.update(loss.item(), input.size(0))
+        top1.update(acc1[0], input.size(0))
+        top5.update(acc5[0], input.size(0))
+
+        # compute gradient and do SGD step
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+
+        # measure elapsed time
+        batch_time.update(time.time() - end)
+        end = time.time()
+
+        if i % args.print_freq == 0:
+            progress.print(i)
 
 
 def validate(val_loader, model, criterion, args):
@@ -239,49 +233,49 @@ def validate(val_loader, model, criterion, args):
     top5 = AverageMeter('Acc@5', ':6.2f')
     progress = ProgressMeter(len(val_loader), batch_time, losses, top1, top5,
                              prefix='Test: ')
-        
-                             # switch to evaluate mode
-                             model.eval()
-                             
-                             with torch.no_grad():
-                                 end = time.time()
-                                     for i, (input, target) in enumerate(val_loader):
-                                         # TODO:
-                                         # input = input.cuda(args.gpu, non_blocking=True)
-                                         # target = target.cuda(args.gpu, non_blocking=True)
-                                         device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
-                                             # input = input.cuda(args.gpu, non_blocking=True)
-                                             # target = target.cuda(args.gpu, non_blocking=True)
-                                             input = input.to(device)
-                                                 target = target.to(device)
-                                                     # compute output
-                                                     output = model(input)
-                                                         loss = criterion(output, target)
-                                                             
-                                                             # measure accuracy and record loss
-                                                             acc1, acc5 = accuracy(output, target, topk=(1, 5))
-                                                                 losses.update(loss.item(), input.size(0))
-                                                                     top1.update(acc1[0], input.size(0))
-                                                                         top5.update(acc5[0], input.size(0))
-                                                                             
-                                                                             # measure elapsed time
-                                                                             batch_time.update(time.time() - end)
-                                                                                 end = time.time()
-                                                                                     
-                                                                                     if i % args.print_freq == 0:
-                                                                                         progress.print(i)
-                                                                                             
-                                                                                             # TODO: this should also be done with the ProgressMeter
-                                                                                             print(' * Acc@1 {top1.avg:.3f} Acc@5 {top5.avg:.3f}'
-                                                                                                   .format(top1=top1, top5=top5))
-                                                                                         
-return top1.avg
+
+    # switch to evaluate mode
+    model.eval()
+
+    with torch.no_grad():
+        end = time.time()
+        for i, (input, target) in enumerate(val_loader):
+            # TODO:   
+            # input = input.cuda(args.gpu, non_blocking=True)
+            # target = target.cuda(args.gpu, non_blocking=True)
+            device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+            # input = input.cuda(args.gpu, non_blocking=True)
+            # target = target.cuda(args.gpu, non_blocking=True)
+            input = input.to(device)
+            target = target.to(device)
+            # compute output
+            output = model(input)
+            loss = criterion(output, target)
+
+            # measure accuracy and record loss
+            acc1, acc5 = accuracy(output, target, topk=(1, 5))
+            losses.update(loss.item(), input.size(0))
+            top1.update(acc1[0], input.size(0))
+            top5.update(acc5[0], input.size(0))
+
+            # measure elapsed time
+            batch_time.update(time.time() - end)
+            end = time.time()
+
+            if i % args.print_freq == 0:
+                progress.print(i)
+
+        # TODO: this should also be done with the ProgressMeter
+        print(' * Acc@1 {top1.avg:.3f} Acc@5 {top5.avg:.3f}'
+              .format(top1=top1, top5=top5))
+
+    return top1.avg
 
 
-def save_checkpoint(state, is_best, filename='CIFAR10_normalization_checkpoint.pth.tar'):
+def save_checkpoint(state, is_best, filename='CIFAR10_baseline_checkpoint.pth.tar'):
     torch.save(state, filename)
     if is_best:
-        shutil.copyfile(filename, 'CIFAR10_normalization_model_best.pth.tar')
+        shutil.copyfile(filename, 'CIFAR10_baseline_checkpoint.pth.tar')
 
 
 class AverageMeter(object):
@@ -290,19 +284,19 @@ class AverageMeter(object):
         self.name = name
         self.fmt = fmt
         self.reset()
-    
+
     def reset(self):
         self.val = 0
         self.avg = 0
         self.sum = 0
         self.count = 0
-    
+
     def update(self, val, n=1):
         self.val = val
         self.sum += val * n
         self.count += n
         self.avg = self.sum / self.count
-    
+
     def __str__(self):
         fmtstr = '{name} {val' + self.fmt + '} ({avg' + self.fmt + '})'
         return fmtstr.format(**self.__dict__)
@@ -313,12 +307,12 @@ class ProgressMeter(object):
         self.batch_fmtstr = self._get_batch_fmtstr(num_batches)
         self.meters = meters
         self.prefix = prefix
-    
+
     def print(self, batch):
         entries = [self.prefix + self.batch_fmtstr.format(batch)]
         entries += [str(meter) for meter in self.meters]
         print('\t'.join(entries))
-    
+
     def _get_batch_fmtstr(self, num_batches):
         num_digits = len(str(num_batches // 1))
         fmt = '{:' + str(num_digits) + 'd}'
@@ -327,7 +321,7 @@ class ProgressMeter(object):
 
 def adjust_learning_rate(optimizer, epoch, args):
     """Sets the learning rate to the initial LR decayed by 10 every 30 epochs"""
-    lr = args.lr * (0.1 ** (epoch // 30))
+    lr = args.lr * (0.1 ** (epoch // 150))
     for param_group in optimizer.param_groups:
         param_group['lr'] = lr
 
@@ -337,11 +331,11 @@ def accuracy(output, target, topk=(1,)):
     with torch.no_grad():
         maxk = max(topk)
         batch_size = target.size(0)
-        
+
         _, pred = output.topk(maxk, 1, True, True)
         pred = pred.t()
         correct = pred.eq(target.view(1, -1).expand_as(pred))
-        
+
         res = []
         for k in topk:
             correct_k = correct[:k].view(-1).float().sum(0, keepdim=True)
