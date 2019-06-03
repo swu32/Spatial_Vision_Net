07e46ae957482abf669e2e5071fc3b27201b2322
wushuc <wushuc@student.ethz.ch>
2019-06-03 16:33:08 +0200

    add resnet module

--- code/model.py
+++ code/model.py
@@ -12,31 +12,34 @@ import torch.utils.model_zoo as model_zoo
 
 
 
+model_urls = {
+    'resnet18': 'https://download.pytorch.org/models/resnet18-5c106cde.pth',
+    'resnet34': 'https://download.pytorch.org/models/resnet34-333f7ec4.pth',
+    'resnet50': 'https://download.pytorch.org/models/resnet50-19c8e357.pth',
+    'resnet101': 'https://download.pytorch.org/models/resnet101-5d3b4d8f.pth',
+    'resnet152': 'https://download.pytorch.org/models/resnet152-b121ed2d.pth',
+}
 
-
-
-
-def conv3x3(in_planes, out_planes, stride=1):
-    """3x3 convolution with padding"""
-    return nn.Conv2d(in_planes, out_planes, kernel_size=3, stride=stride,
-                     padding=1, bias=False)
 
 
 class BasicBlock(nn.Module):
     expansion = 1
 
-    def __init__(self, inplanes, planes, stride=1, downsample=None):
+    def __init__(self, inplanes, planes, stride=1, downsample=None, norm_layer=None):
         super(BasicBlock, self).__init__()
+        if norm_layer is None:
+            norm_layer = nn.BatchNorm2d
+        # Both self.conv1 and self.downsample layers downsample the input when stride != 1
         self.conv1 = conv3x3(inplanes, planes, stride)
-        self.bn1 = nn.BatchNorm2d(planes)
+        self.bn1 = norm_layer(planes)
         self.relu = nn.ReLU(inplace=True)
         self.conv2 = conv3x3(planes, planes)
-        self.bn2 = nn.BatchNorm2d(planes)
+        self.bn2 = norm_layer(planes)
         self.downsample = downsample
         self.stride = stride
 
     def forward(self, x):
-        residual = x
+        identity = x
 
         out = self.conv1(x)
         out = self.bn1(out)
@@ -46,25 +49,58 @@ class BasicBlock(nn.Module):
         out = self.bn2(out)
 
         if self.downsample is not None:
-            residual = self.downsample(x)
+            identity = self.downsample(x)
 
-        out += residual
+        out += identity
         out = self.relu(out)
 
         return out
 
+# the previous basic block
+# class BasicBlock(nn.Module):
+#     expansion = 1
 
-class V1_ResNet(nn.Module):
+#     def __init__(self, inplanes, planes, stride=1, downsample=None):
+#         super(BasicBlock, self).__init__()
+#         self.conv1 = conv3x3(inplanes, planes, stride)
+#         self.bn1 = nn.BatchNorm2d(planes)
+#         self.relu = nn.ReLU(inplace=True)
+#         self.conv2 = conv3x3(planes, planes)
+#         self.bn2 = nn.BatchNorm2d(planes)
+#         self.downsample = downsample
+#         self.stride = stride
+
+#     def forward(self, x):
+#         residual = x
+
+#         out = self.conv1(x)
+#         out = self.bn1(out)
+#         out = self.relu(out)
+
+#         out = self.conv2(out)
+#         out = self.bn2(out)
+
+#         if self.downsample is not None:
+#             residual = self.downsample(x)
+
+#         out += residual
+#         out = self.relu(out)
+
+#         return out
+
+
+class Spatial_Vision_Net(nn.Module):
+    '''Main Class for Spatial Vision Net'''
 
     def __init__(self, block, layers, low_freq = False, num_classes=1000, n_img_per_batch = 4, n_freq  = 12, n_orient = 8, n_phase = 2, imsize = 224):
         self.inplanes = 64
-        super(V1_ResNet, self).__init__()
+        super(Spatial_Vision_Net, self).__init__()
         self.n_img_per_batch = n_img_per_batch
         self.n_freq = n_freq
         self.n_orient = n_orient
         self.n_phase = n_phase
         self.imsize = imsize
-        if low_freq:
+        if low_freq: # only employs half of the spatial frequency filters
             self.v1 = V1_Low_Frequency_net(n_img_per_batch = self.n_img_per_batch, n_freq  = 6, n_orient = self.n_orient, n_phase = self.n_phase, imsize = self.imsize)
         else:
             self.v1 = V1_Imagenet_net(n_img_per_batch = self.n_img_per_batch, n_freq  = self.n_freq, n_orient = self.n_orient, n_phase = self.n_phase, imsize = self.imsize)
@@ -154,7 +190,7 @@ class mean_padding(torch.nn.Module): # checked
 class log_Gabor_convolution(torch.nn.Module): # checked
     def __init__(self, sz, n_img_per_batch, low_freq = False):
         """
-        include declarations that can be prespeified. 
+        low_freq: only use half of the filters starting with the lowest frequency
         """
         super(log_Gabor_convolution, self).__init__()
         pad_l = int(sz/2) # pad half of the image size. 
@@ -175,25 +211,22 @@ class log_Gabor_convolution(torch.nn.Module): # checked
         n_filters = n_freq*n_orient*2 # multiply by phase
         # combine real and imagary filters
         if not low_freq:
-            s = 0
-            filter_banks = np.zeros([n_filters,1,sz,sz]) # 1 is left for the gray value channel
-
-            for f in range(0,n_freq):
-                for o in range(0, n_orient):
-                    filter_banks[s,0,:,:] = spatial_filters_real[f,o] - np.mean(spatial_filters_real[f,o])
-                    s = s + 1
-                    filter_banks[s,0,:,:] = spatial_filters_imag[f,o] - np.mean(spatial_filters_imag[f,o])
-                    s = s + 1
+            banksize = n_filters
+            start = 0
         else: 
-            s = 0
-            filter_banks = np.zeros([int(n_filters/2),1,sz,sz]) # 1 is left for the gray value channel
-            for f in range(int(n_freq/2),n_freq):
-                for o in range(0, n_orient):
-                    filter_banks[s,0,:,:] = spatial_filters_real[f,o] - np.mean(spatial_filters_real[f,o])
-                    s = s + 1
-                    filter_banks[s,0,:,:] = spatial_filters_imag[f,o] - np.mean(spatial_filters_imag[f,o])
-                    s = s + 1
+            banksize = int(n_filters/2)
+            start = int(n_freq/2)
 
+        # modified code: 
+        filter_banks = np.zeros([banksize,1,sz,sz]) # 1 is left for the gray value channel
+        s = 0
+
+        for f in range(start,n_freq):
+            for o in range(0, n_orient):
+                filter_banks[s,0,:,:] = spatial_filters_real[f,o] - np.mean(spatial_filters_real[f,o])
+                s = s + 1
+                filter_banks[s,0,:,:] = spatial_filters_imag[f,o] - np.mean(spatial_filters_imag[f,o])
+                s = s + 1
 
         filter_banks = torch.FloatTensor(filter_banks)
         # filter_banks = filter_banks.type(torch.FloatTensor)
@@ -325,13 +358,13 @@ def v1resnet18(pretrained=False, **kwargs):
     """
     device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
    
-    model = V1_ResNet(BasicBlock, [2, 2, 2, 2], **kwargs)
+    model = Spatial_Vision_Net(BasicBlock, [2, 2, 2, 2], **kwargs)
     model.to(device)
     return model
 
 def low_freq_resnet18(pretrained=False, **kwargs):
     device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
-    model = V1_ResNet(BasicBlock, [2, 2, 2, 2], low_freq = True, **kwargs)
+    model = Spatial_Vision_Net(BasicBlock, [2, 2, 2, 2], low_freq = True, **kwargs)
     model.to(device)
     return model
 
@@ -394,4 +427,144 @@ class V1_Low_Frequency_net(nn.Module):
 
         return R
 
+
+
+"""ResNet related functions"""
+def conv3x3(in_planes, out_planes, stride=1):
+    """3x3 convolution with padding"""
+    return nn.Conv2d(in_planes, out_planes, kernel_size=3, stride=stride,
+                     padding=1, bias=False)
+
+
+def conv1x1(in_planes, out_planes, stride=1):
+    """1x1 convolution"""
+    return nn.Conv2d(in_planes, out_planes, kernel_size=1, stride=stride, bias=False)
+
+
+class Bottleneck(nn.Module):
+    expansion = 4
+
+    def __init__(self, inplanes, planes, stride=1, downsample=None, norm_layer=None):
+        super(Bottleneck, self).__init__()
+        if norm_layer is None:
+            norm_layer = nn.BatchNorm2d
+        # Both self.conv2 and self.downsample layers downsample the input when stride != 1
+        self.conv1 = conv1x1(inplanes, planes)
+        self.bn1 = norm_layer(planes)
+        self.conv2 = conv3x3(planes, planes, stride)
+        self.bn2 = norm_layer(planes)
+        self.conv3 = conv1x1(planes, planes * self.expansion)
+        self.bn3 = norm_layer(planes * self.expansion)
+        self.relu = nn.ReLU(inplace=True)
+        self.downsample = downsample
+        self.stride = stride
+
+    def forward(self, x):
+        identity = x
+
+        out = self.conv1(x)
+        out = self.bn1(out)
+        out = self.relu(out)
+
+        out = self.conv2(out)
+        out = self.bn2(out)
+        out = self.relu(out)
+
+        out = self.conv3(out)
+        out = self.bn3(out)
+
+        if self.downsample is not None:
+            identity = self.downsample(x)
+
+        out += identity
+        out = self.relu(out)
+
+        return out
+
+
+class ResNet(nn.Module):
+
+    def __init__(self, block, layers, num_classes=1000, zero_init_residual=False, norm_layer=None):
+        super(ResNet, self).__init__()
+        if norm_layer is None:
+            norm_layer = nn.BatchNorm2d
+        self.inplanes = 64
+        self.conv1 = nn.Conv2d(3, 64, kernel_size=7, stride=2, padding=3,
+                               bias=False)
+        self.bn1 = norm_layer(64)
+        self.relu = nn.ReLU(inplace=True)
+        self.maxpool = nn.MaxPool2d(kernel_size=3, stride=2, padding=1)
+        self.layer1 = self._make_layer(block, 64, layers[0], norm_layer=norm_layer)
+        self.layer2 = self._make_layer(block, 128, layers[1], stride=2, norm_layer=norm_layer)
+        self.layer3 = self._make_layer(block, 256, layers[2], stride=2, norm_layer=norm_layer)
+        self.layer4 = self._make_layer(block, 512, layers[3], stride=2, norm_layer=norm_layer)
+        self.avgpool = nn.AdaptiveAvgPool2d((1, 1))
+        self.fc = nn.Linear(512 * block.expansion, num_classes)
+
+        for m in self.modules():
+            if isinstance(m, nn.Conv2d):
+                nn.init.kaiming_normal_(m.weight, mode='fan_out', nonlinearity='relu')
+            elif isinstance(m, (nn.BatchNorm2d, nn.GroupNorm)):
+                nn.init.constant_(m.weight, 1)
+                nn.init.constant_(m.bias, 0)
+
+        # Zero-initialize the last BN in each residual branch,
+        # so that the residual branch starts with zeros, and each residual block behaves like an identity.
+        # This improves the model by 0.2~0.3% according to https://arxiv.org/abs/1706.02677
+        if zero_init_residual:
+            for m in self.modules():
+                if isinstance(m, Bottleneck):
+                    nn.init.constant_(m.bn3.weight, 0)
+                elif isinstance(m, BasicBlock):
+                    nn.init.constant_(m.bn2.weight, 0)
+
+    def _make_layer(self, block, planes, blocks, stride=1, norm_layer=None):
+        if norm_layer is None:
+            norm_layer = nn.BatchNorm2d
+        downsample = None
+        if stride != 1 or self.inplanes != planes * block.expansion:
+            downsample = nn.Sequential(
+                conv1x1(self.inplanes, planes * block.expansion, stride),
+                norm_layer(planes * block.expansion),
+            )
+
+        layers = []
+        print([self.inplanes, planes, stride,downsample,norm_layer])
+
+        layers.append(block(self.inplanes, planes, stride, downsample, norm_layer))
+ 
+        self.inplanes = planes * block.expansion
+        for _ in range(1, blocks):
+
+            layers.append(block(self.inplanes, planes, norm_layer=norm_layer))
+
+        return nn.Sequential(*layers)
+
+    def forward(self, x):
+        x = self.conv1(x)
+        x = self.bn1(x)
+        x = self.relu(x)
+        x = self.maxpool(x)
+
+        x = self.layer1(x)
+        x = self.layer2(x)
+        x = self.layer3(x)
+        x = self.layer4(x)
+
+        x = self.avgpool(x)
+        x = x.view(x.size(0), -1)
+        x = self.fc(x)
+
+        return x
+
+def resnet18(pretrained=False, **kwargs):
+    """Constructs a ResNet-18 model.
+
+    Args:
+        pretrained (bool): If True, returns a model pre-trained on ImageNet
+    """
+    model = ResNet(BasicBlock, [2, 2, 2, 2], **kwargs)
+    if pretrained:
+        model.load_state_dict(model_zoo.load_url(model_urls['resnet18']))
+    return model
 
